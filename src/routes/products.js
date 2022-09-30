7bfeaa83ac1bf7e18b0364f03753c7202f9fbd09
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-09-30 19:47:38 -0300

    Obtengo lista de productos

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 const products = require('../schemes/products');
 
+//Endpoints
+
+//GET: para conseguir la lista entera de productos
+router.get('/', (req, res) =>{
+    products.find({isDeleted: false})
+    .then(data => res.status(200).json(data))
+    .catch(error => res.status(500).json({mensaje: error}))
+})
+
+
+
+
+
 module.exports = router;
