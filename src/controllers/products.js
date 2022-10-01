2027c738c8a6b200df4ff3de490ea0522ca14512
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-10-01 11:18:32 -0300

    Agrego endpoints delete y update

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -25,12 +25,35 @@ searchProduct = (req, res) =>{
 //POST: para agregar un producto a la BD
 addProduct = (req, res) => {
     const newProducts = new products(req.body);
-    newProducts
-    .save()
-    .then(data => res.status(201).json({mensaje: "Producto añadido", data}))
+    newProducts.save()
+    .then(data => res.status(201).json({mensaje: `El producto ${data.name} ha sido añadido`, data}))
     .catch(() => res.status(500).json({mensaje: "Error"}));
 }
 
 //DELETE: para eliminar un producto
+deleteProduct = (req, res) => {
+    const id = req.params.id;
+    products.findByIdAndDelete(id)
+    .then(data => {
+        if (!data) {
+            return res.status(404).json({mensaje: "Producto no encontrado"});
+        }
+        return res.status(200).json({mensaje: `El producto ${data.name} ha sido eliminado`});
+    })
+    .catch(error => res.status(500).json({error}));
+}
 
-module.exports = { listProducts, searchProduct, addProduct };
+//PUT: para editar algún campo de un producto
+updateProduct = (req, res) => {
+    const id = req.params.id;
+    products.findByIdAndUpdate(id, req.body)
+    .then(data => {
+        if(!data){
+            return res.status(404).json({mensaje: "Producto no encontrado"})
+        }
+        return res.status(200).json({mensaje: `El producto ${data.name} ha sido actualizado`});
+    })
+    .catch(error => res.status(500).json({error}));
+}
+
+module.exports = { listProducts, searchProduct, addProduct, deleteProduct, updateProduct };
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
+require('../controllers/products');
 
-const { getProducts, searchProduct, addProduct } = require('../controllers/products');
+router.get('/', (req, res) => listProducts(req, res));
 
-router.get('/', (req, res) => listProducts(req, res))
+router.get('/:name', (req, res) => searchProduct(req, res));
 
-router.get('/:name', (req,res) => searchProduct(req, res))
+router.post("/add", (req, res) => addProduct(req, res));
 
-router.post("/add", (req,res) => addProduct(req, res))
+router.delete("/delete/:id", (req, res) => deleteProduct(req, res));
+
+router.put("/update/:id", (req, res) => updateProduct(req, res));
 
 module.exports = router;
